0e01ab5a6a21308e70299e7933f691d5d2d50299
TristanBruel <tristan.bruel@unimib.it>
2025-11-26 11:47:55 +0100

    change name of caseMT in bpp_array.f

--- src/cosmic/src/bpp_array.f
+++ src/cosmic/src/bpp_array.f
@@ -10,7 +10,8 @@
      &                      b_0_1,b_0_2,bacc1,bacc2,tacc1,tacc2,
      &                      epoch1,epoch2,bhspin1,bhspin2,
      &                      deltam_1,deltam_2,SN_1,SN_2,
-     &                      bin_state,merger_type,case1,case2,
+     &                      caseMT_1,caseMT_2,
+     &                      bin_state,merger_type,
      &                      tabname)
         IMPLICIT NONE
         INCLUDE 'const_bse.h'
@@ -30,7 +31,7 @@
         REAL*8 bhspin1,bhspin2,teff1,teff2
         REAL*8 deltam_1,deltam_2
         INTEGER SN_1,SN_2,bin_state,merger_type
-        INTEGER case1, case2
+        INTEGER caseMT_1, caseMT_2
         REAL*8 tb_write,sep_cubed
         INTEGER jp, col_ind
         INTEGER kstar1,kstar2
@@ -94,8 +95,8 @@
         all_cols(45) = deltam_2
         all_cols(46) = float(SN_1)
         all_cols(47) = float(SN_2)
-        all_cols(48) = case1
-        all_cols(49) = case2
+        all_cols(48) = caseMT_1
+        all_cols(49) = caseMT_2
         all_cols(50) = bin_state
         all_cols(51) = merger_type
 
